32dcbe0a97260546ef20371970c5e307b2ff5d5f
Jmestrelozano <jmestrelozano@gmail.com>
2021-11-14 11:39:47 -0500

    creando proyecto de laika prueba

---
+++ android/app/src/main/java/com/laika_prueba/MainActivity.java
@@ -0,0 +1,15 @@
+package com.laika_prueba;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "laika_prueba";
+  }
+}
